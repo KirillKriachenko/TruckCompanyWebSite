e996357d15d1e77042cde788b4885e20c68d0676
fabiociconi <fabiociconi@yahoo.com.br>
2017-03-02 20:00:21 -0300

    preview master

--- TruckWebSite/TruckWebSite/MasterPage.Master.designer.cs
+++ TruckWebSite/TruckWebSite/MasterPage.Master.designer.cs
@@ -31,6 +31,15 @@ namespace TruckWebSite {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// headerimage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image headerimage;
+        
+        /// <summary>
         /// menu control.
         /// </summary>
         /// <remarks>
