947362066ebadcf0e1b852c35aa7bb02d1635658
fabiociconi <fabiociconi@yahoo.com.br>
2017-03-31 18:05:09 -0300

    31/03/2017

--- TruckWebSite/TruckWebSite/Default.aspx
+++ TruckWebSite/TruckWebSite/Default.aspx
@@ -1,26 +1,34 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TruckWebSite.Default" %>
-
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
+
 <asp:Content ID="Content2" ContentPlaceHolderID="body" runat="server">
+    <%--Mini-form to select type of truck--%>
     <div id="Miniform">
+        <%--Title--%>
         <asp:Label ID="LabelBuy" runat="server" CssClass="labels" Text="Buy Your Truck!!!!"></asp:Label><br />
+        <%--Plate--%>
         <asp:DropDownList ID="DropDownList" CssClass="boxes" runat="server" enableViewstate="false" AutoPostBack="false" >
-            <asp:ListItem>Select...</asp:ListItem>
+            <asp:ListItem Value="0">Select...</asp:ListItem>
         </asp:DropDownList><br />
+        <%--Year--%>
         <asp:DropDownList ID="DropDownList1" CssClass="boxes" runat="server" enableViewstate="false" AutoPostBack="false">
             <asp:ListItem>Year...</asp:ListItem>
         </asp:DropDownList><br />
+        <%--Brand--%>
         <asp:DropDownList ID="DropDownList2" CssClass="boxes" runat="server" enableViewstate="false" AutoPostBack="false">
             <asp:ListItem>Brand...</asp:ListItem>
         </asp:DropDownList><br />
+        <%--Location--%>
         <asp:DropDownList ID="DropDownList3" CssClass="boxes" runat="server" enableViewstate="false" AutoPostBack="false">
             <asp:ListItem>All Location...</asp:ListItem>
-        </asp:DropDownList><br />
+        </asp:DropDownList><br /> 
+        <%--choose between new or old truck--%>
         <asp:Label ID="LabelNew" runat="server" Text="New" ForeColor="White"></asp:Label>
         <asp:RadioButton ID="RadioButtonNew" runat="server" GroupName="MeasurementSystem" />
         <asp:Label ID="LabelUsed" runat="server" Text="Used" ForeColor="White"></asp:Label>
         <asp:RadioButton ID="RadioButtonUsed" runat="server" GroupName="MeasurementSystem" />
+        <%--search button--%>
         <asp:Button ID="ButtonSeach" runat="server" CssClass="btn btn-default" Text="Search" OnClick="ButtonSeach_Click" />
         <asp:Label ID="LabelWarnig" runat="server" CssClass="alert-info" Visible="false" Text="Choose again..."></asp:Label>
     </div>
--- TruckWebSite/TruckWebSite/Default.aspx.cs
+++ TruckWebSite/TruckWebSite/Default.aspx.cs
@@ -10,56 +10,77 @@ using TruckWebSite.DB;
 namespace TruckWebSite
 {
     /// <summary>
-    /// 
+    ///  Main page of Company Truck's website
+    ///  by Fabio Alexandre Ciconi
     /// </summary>
     public partial class Default : System.Web.UI.Page
     {
         /// <summary>
-        /// 
+        /// LOAD PAGE 
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         protected void Page_Load(object sender, EventArgs e)
         {
+            /*WARNING MESSAGE TO INVISIBLE*/
             LabelWarnig.Visible = false;
 
+            /*CHECK IF NOT PAGE POSTBACK POPULATE THE DROPDOWNLISTS*/
             if (!Page.IsPostBack)
                 PopularDropDownList();
+
         }
+
         /// <summary>
-        /// 
+        /// This method has the function to populate the mini-form with information provides by databases
+        /// database name:TRUCKCOMPANYDB
         /// </summary>
         private void PopularDropDownList()
         {
             using (var db = new TruckCompanyDBContainer())
             {
-                var query = from b in db.CarSet
-                            join f in db.BrancheSet on b.BrancheBrancheId equals f.BrancheId into prodGroup
-                            select new { b.Branche, b.Brand, b.Year, b.Plate};
+                var query = from b in db.CarSet//CAR TABLE
+                            join f in db.BrancheSet // JOIN WITH BRACHES TABLE
+                            on b.BrancheBrancheId equals f.BrancheId 
+                            into prodGroup/**********/
+                            select new { b.Branche, b.Brand, b.Year, b.Plate};//CONTENT SELECTED
+                
+                /*LOOP TO POPULATE THE DROPDOWNLIST*/                        
                 foreach (var item in query)
                 {
+                    
+                    /*PLATE*/
                     DropDownList.Items.Add(new ListItem(item.Plate));
+                    /*YEAR*/
                     DropDownList1.Items.Add(new ListItem(item.Year.ToString()));
+                    /*BRAND*/
                     DropDownList2.Items.Add(new ListItem(item.Brand.ToString()));
-                    DropDownList3.Items.Add(new ListItem(item.Branche.Location));                   
+                    /*LOCATION*/
+                    DropDownList3.Items.Add(new ListItem(item.Branche.Location));
+
+                    
+           
                 }
             }            
         }
 
         /// <summary>
-        /// 
+        /// THIS METHOD TAKES ALL INFORMATIONS THAT WAS SELECTED BY THE USER AND VALIDATES THEM.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         protected void ButtonSeach_Click(object sender, EventArgs e)
         {
             //Pegar o valor do item do DropDownList
-            //string plate = DropDownList.SelectedValue.ToString();
-            string plate = DropDownList.SelectedItem.ToString();
-            string Year = DropDownList.SelectedValue.ToString();
-            string Brand = DropDownList.SelectedValue.ToString();
-            string Branche = DropDownList.DataTextField.ToString();
+            
+            /*LOCAL VARIABLES*/
+            string plate    = DropDownList.SelectedItem.ToString();
+            string TEST     = DropDownList.SelectedItem.Enabled.ToString();
+            string Year     = DropDownList.SelectedValue.ToString();
+            string Brand    = DropDownList.SelectedValue.ToString();
+            string Branche  = DropDownList.DataTextField.ToString();
 
+            /*WE HAVE ONLY ONE TYPE OF TRUCK THAT IS BE ACCEPT*/
             if (plate == "X2LL2EF" && Year == "2014" &&
                 Brand == "VOLKSWAGEN" && Branche == "TORONTO" && RadioButtonUsed.Checked)
             {
@@ -68,6 +89,7 @@ namespace TruckWebSite
             }
             else
             {
+                /*WARNING MESSAGE*/
                 LabelWarnig.Visible = true;
             }
         }
